718b65b0ee872c3453aa13a177ba9c3fe7ef1c22
YoussefCHOUNI <chouniy@yahoo.fr>
2020-04-14 23:22:40 +0100

    commite1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# les-filtres-de-recherche-en-JS
+﻿# les-filtres-de-recherche-en-JS
  les méthodes de liste, fondements du JS
+
+#Apports pédagogiques :
+
+- méthodes de liste, fondements du JS
+- Array.prototype.filter()
+- Array.prototype.map()
+- Array.prototype.sort()
+- Array.prototype.reduce()
+
+#Travail Demander 
+
+en utilisant les fonctions en haut vous devez réaliser un script JS 
+pour chercher les femmes inventrices par spécialité puis par Siècle, 
+avec l'utilisation d'une liste déroulante ou recherche par mot.
---
+++ script.js
@@ -0,0 +1,110 @@
+const inventors = [{
+        first: 'Marie',
+        last: 'Curie',
+        career: 'physicist',
+        category: 'Physics, Chemistry',
+        bio: "Physicienne et chimiste polonaise, naturalisée française, reconnue pour la découverte de l'opium et du polonium.",
+        year: 1867,
+        passed: 1934
+    },
+    {
+        first: 'Ada',
+        last: 'Lovelace',
+        career: 'computing pioneer',
+        category: 'Informatics',
+        bio: 'Comtesse anglaise, pionière de la science informatique.',
+        year: 1815,
+        passed: 1852
+    },
+    {
+        first: 'Maria',
+        last: 'Telkes',
+        career: 'bio-physicist',
+        category: 'Physics, Chemistry',
+        bio: "Scientifique américano-hongroise et inventrice ayant travaillé sur les technologies liées à l'énergie solaire.",
+        year: 1900,
+        passed: 1995
+    },
+    {
+        first: 'Grace',
+        last: 'Hopper',
+        career: 'mathematician and programmer',
+        category: 'Mathematics, Informatics',
+        bio: "Informaticienne américaine et Rear admiral de la marine américaine. Recoonue pour la conception du premier compilateur en 1951 (A-0 System) et du langage COBOL en 1959.",
+        year: 1906,
+        passed: 1992
+    },
+    {
+        first: 'Shirley Ann',
+        last: 'Jackson',
+        career: 'physicist',
+        category: 'Physics',
+        bio: "Physicienne américaine, spécialisée dans le domaine de la physique des matières condensées et des matériaux opto-électroniques.",
+        year: 1946,
+        passed: d.getFullYear()
+    },
+    {
+        first: 'Rosalind',
+        last: 'Franklin',
+        career: 'physical chemist',
+        category: 'Physics, Chemistry',
+        bio: "Physico-chimiste, pionnière de la biologie moléculaire britannique ayant participé de manière déterminante à la découverte de la structure de l'acide désoxyribonucléique (ADN) et du virus de la mosaïque du tabac.",
+        year: 1920,
+        passed: 1958
+    },
+    {
+        first: 'Henrietta',
+        last: 'Swan Leavitt',
+        career: 'astronom',
+        category: 'Engineering',
+        bio: "Astronome américaine ayant découvert la relation entre la luminosité des étoiles variables et leur période de variation.",
+        year: 1868,
+        passed: 1921
+    },
+    {
+        first: 'Grete',
+        last: 'Hermann',
+        career: 'mathematician',
+        category: 'Mathematics',
+        bio: "Mathématicienne et philosophe allemande connue pour ses travaux en mathématiques, en physique, en philosophie et en éducation.",
+        year: 1901,
+        passed: 1984
+    },
+    {
+        first: 'Gertrude',
+        last: 'Blanch',
+        career: 'mathematician',
+        category: 'Mathematics',
+        bio: "Mathématicienne américaine, d'origine polonaise, ayant réalisé des études novatrices en analyse numérique et en informatique.",
+        year: 1897,
+        passed: 1996
+    },
+    {
+        first: 'Ida',
+        last: 'Rhodes',
+        career: 'mathematician and programmer',
+        category: 'Mathematics, Informatics',
+        bio: "Mathématicienne américaine d'origine ukrainienne, une des femmes au coeur du développement des prémices de l'ordinateur.",
+        year: 1900,
+        passed: 1986
+    },
+    {
+        first: 'Margaret',
+        last: 'Hamilton',
+        career: 'mathematician (Apollo)',
+        category: 'Mathematics',
+        bio: "Informaticienne et mathématicienne américaine. Elle était directrice du département génie logiciel du MIT Instrumentation Laboratory qui conçut le système embarqué du programme Apollo.",
+        year: 1936,
+        passed: d.getFullYear()
+    }
+];
+
+const people = ['Dimple, Halloran', 'Hannah, Luhman', 'Latonya, Torres',
+    'Sana, Kitt', 'Dedra, Re', 'Betsy, Royalty', 'Jaimee, Goldner',
+    'Verline, Chubb', 'Adria, Loux', 'Carolee, Parrish', 'Judy, Broadhead',
+    'Bradley, Buitron', 'Antony, Burlison', 'Meggan, Timoteo', 'Chanell, Zeller',
+    'Detra, Tsui', 'Roberta, Tullius', 'Phylis, Thibodeaux', 'Augusta, Charley',
+    'Michelle, South', 'Elmer, Demo', 'Malinda, Dipiazza', 'Vivien, Wigley',
+    'Almeta, Jasmin', 'Ricardo, Smail', 'Jon, Mcgranahan', 'Evette, Maus',
+    'Dominque, Hilty', 'Clayton, Finnegan', 'Ethel, Rendell'
+];
